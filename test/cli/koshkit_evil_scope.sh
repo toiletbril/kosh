9dac0f56a394ee232e3b0cc61cf94d67edac2e5d
toiletbril <s9und@outlook.com>
2026-09-13 19:41:43 +0300

    Verify Evil output scopes

---
+++ test/cli/koshkit_evil_scope.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+default_report=$($BIN -c 'koshkit --color never evil')
+case $default_report in
+  *'Users:'*) default_users=expanded ;;
+  *) default_users=momentary ;;
+esac
+printf 'default-users=%s\n' "$default_users"
+case $default_report in
+  *PROCFS*|*ANOMALIES*) default_extended=extra ;;
+  *) default_extended=omitted ;;
+esac
+printf 'default-extended=%s\n' "$default_extended"
+
+users_report=$($BIN -c 'koshkit --color never evil --users')
+case $users_report in
+  *'Users:'*) users_selector=matched ;;
+  *) users_selector=missing ;;
+esac
+printf 'users-selector=%s\n' "$users_selector"
+
+all_report=$($BIN -c 'koshkit --color never evil --all')
+case $all_report in
+  *PROCFS*ANOMALIES*) all_extended=matched ;;
+  *) all_extended=missing ;;
+esac
+printf 'all-extended=%s\n' "$all_extended"
+
+short_report=$($BIN -c 'koshkit --color never evil --short')
+case $short_report in
+  *Uptime*|*PROCFS*|*ANOMALIES*) short_scope=extra ;;
+  *) short_scope=identity ;;
+esac
+printf 'short-scope=%s\n' "$short_scope"
